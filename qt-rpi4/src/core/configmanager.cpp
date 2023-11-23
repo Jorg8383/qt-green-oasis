a4326996babb5123c3bb64ed4ea02da48a5a4090
Jorg8383 <joerg.striebel@icloud.com>
2023-11-23 17:37:35 +0100

    feat(ConfigManager): define methods and variables for the config manager class

--- qt-rpi4/src/core/configmanager.cpp
+++ qt-rpi4/src/core/configmanager.cpp
@@ -1,7 +1,12 @@
 #include "configmanager.h"
 
-ConfigManager::ConfigManager(QObject *parent)
+ConfigManager::ConfigManager(const QString &configFileName, QObject *parent)
     : QObject{parent}
 {
 
+}
+
+QVariant ConfigManager::getValue(const QString &key, bool fromEnv) const
+{
+    return 0;
 }
--- qt-rpi4/src/core/configmanager.h
+++ qt-rpi4/src/core/configmanager.h
@@ -4,15 +4,20 @@
 #include <QObject>
 #include <QFile>
 #include <QTextStream>
+#include <QSettings>
 
 class ConfigManager : public QObject
 {
     Q_OBJECT
 public:
-    explicit ConfigManager(QObject *parent = nullptr);
+    explicit ConfigManager(const QString &configFileName = "", QObject *parent = nullptr);
+
+    QVariant getValue(const QString &key, bool fromEnv = false) const;
 
 signals:
 
+private:
+    QSettings m_settings;
 };
 
 #endif // CONFIGMANAGER_H

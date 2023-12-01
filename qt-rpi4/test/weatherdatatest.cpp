cd892e82c9da90e06c744e5847169b0181435b1a
Jorg8383 <joerg.striebel@icloud.com>
2023-12-01 10:31:37 +0100

    test(weather): create JSON test data set #1

--- qt-rpi4/test/weatherdatatest.cpp
+++ qt-rpi4/test/weatherdatatest.cpp
@@ -6,3 +6,90 @@ WeatherDataTest::WeatherDataTest(QObject *parent)
 
 }
 
+void WeatherDataTest::testConstructor()
+{
+
+
+}
+
+void WeatherDataTest::testConstructor_data()
+{
+
+    // Prepare JSON test data set #1 - including all properties
+    QJsonObject testDataSet1;
+    testDataSet1.insert("dt", 1701421200);
+    QJsonObject mainObject;
+    mainObject.insert("temp", 20.3);
+    mainObject.insert("temp_min", 18.8);
+    mainObject.insert("temp_max", 23.4);
+    testDataSet1.insert("main", mainObject);
+    QJsonArray weatherArray;
+    QJsonObject weatherObject;
+    weatherObject.insert("id", 500);
+    weatherObject.insert("main", "Rain");
+    weatherObject.insert("description", "light rain");
+    weatherObject.insert("icon", "10d");
+    weatherArray.append(weatherObject);
+    testDataSet1.insert("weather", weatherObject);
+    QJsonObject windObject;
+    windObject.insert("speed", 1.97);
+    testDataSet1.insert("wind", windObject);
+    QJsonObject rainObject;
+    rainObject.insert("3h", 0.57);
+    testDataSet1.insert("rain", rainObject);
+    QJsonObject snowObject;
+    snowObject.insert("3h", 0.52);
+    testDataSet1.insert("snow", snowObject);
+    testDataSet1.insert("pop", 0.33);
+}
+
+void WeatherDataTest::testProperties()
+{
+
+}
+
+void WeatherDataTest::testProperties_data()
+{
+
+}
+
+void WeatherDataTest::initTestCase()
+{
+
+}
+
+void WeatherDataTest::initTestCase_data()
+{
+    // Create a global test table
+    QTest::addColumn<QJsonObject>("data");
+    QTest::addColumn<bool>("isCurrentWeather");
+    QTest::addColumn<int>("dt");
+    QTest::addColumn<QDateTime>("qDateTime");
+    QTest::addColumn<QString>("cityName");
+    QTest::addColumn<QString>("weatherId");
+    QTest::addColumn<QString>("weatherMain");
+    QTest::addColumn<QString>("weatherDescription");
+    QTest::addColumn<QString>("weatherIcon");
+    QTest::addColumn<double>("mainTemp");
+    QTest::addColumn<double>("mainTempMin");
+    QTest::addColumn<double>("mainTempMax");
+    QTest::addColumn<double>("windSpeed");
+    QTest::addColumn<double>("snow3h");
+    QTest::addColumn<double>("rain3h");
+    QTest::addColumn<int>("pop");
+}
+
+void WeatherDataTest::cleanupTestCase()
+{
+
+}
+
+void WeatherDataTest::init()
+{
+
+}
+
+void WeatherDataTest::cleanup()
+{
+
+}
--- qt-rpi4/test/weatherdatatest.h
+++ qt-rpi4/test/weatherdatatest.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 #include <QTest>
+#include <QJsonObject>
+#include <QDateTime>
+#include <QJsonArray>
 #include <weatherdata.h>
 
 class WeatherDataTest : public QObject
@@ -14,6 +17,12 @@ public:
 signals:
 
 private slots:
+    // Define test functions
+    void testConstructor();
+    void testConstructor_data();
+    void testProperties();
+    void testProperties_data();
+
     // Define methodes that are automatically invoked by the test framework
     void initTestCase(); // Will be called before the first test function is executed
     void initTestCase_data(); // Will be called to create a global test data table
@@ -21,11 +30,6 @@ private slots:
     void init(); // Will be called after the last test function was executed
     void cleanup(); // Will be called after the last test function was executed
 
-    void testConstructor();
-    void testConstructor_data();
-
-    void testProperties();
-    void testProperties_data();
 };
 
 #endif // WEATHERDATATEST_H

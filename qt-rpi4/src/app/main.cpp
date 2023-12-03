f4a7b874db37382e3fbc18e18c84c8665423f725
Jorg8383 <joerg.striebel@icloud.com>
2023-12-03 20:48:05 +0100

    build: set new QTP0001 policy which was introduced in Qt6.5

--- qt-rpi4/src/app/main.cpp
+++ qt-rpi4/src/app/main.cpp
@@ -7,8 +7,13 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/qt-rpi4/Main.qml"_qs);
-//    const QUrl url(u"qrc:/qt-rpi4/src/Main.qml"_qs); // do we need to add /src/ ???
+    // Follow the new URL policy introduced in Qt6.5, where ':/qt/qml/' is the default resource prefix for QML modules.
+    const QUrl url(u"qrc:/qt/qml/qt-rpi4/Main.qml"_qs);
+
+/* This commented out URL below follows the previous policy used in version prior to Qt6.5
+ * see app/CMakeLists.txt -> qt_policy(SET QTP0001 NEW)
+ * https://doc.qt.io/qt-6/qt-cmake-policy-qtp0001.html */
+//    const QUrl url(u"qrc:/qt-rpi4/Main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);

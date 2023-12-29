92c4f70721b028a2d942f4c7eed80b7a9c82f807
Jorg8383 <joerg.striebel@icloud.com>
2023-12-29 10:48:32 +0100

    test(weatherfetcher): add q_object to the MockNetworkReply class

--- qt_rpi4/test/mocknetworkreply.h
+++ qt_rpi4/test/mocknetworkreply.h
@@ -2,13 +2,13 @@
 #define MOCKNETWORKREPLY_H
 
 #include <QtTest>
-#include <QSignalSpy>
-#include <QNetworkAccessManager>
 #include <QNetworkReply>
 
 // This class is used for mocking the network reply when testing the WeatherFetcher class
 class MockNetworkReply : public QNetworkReply
 {
+    Q_OBJECT
+
 public:
     MockNetworkReply(const QByteArray &data, QObject *parent = nullptr);
 
